91052f44e22ab568d0732c96022fe9e7f4b75bde
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-13 13:21:29 +0200

    Update contactModel.js

--- src/dbModels/contactModel.js
+++ src/dbModels/contactModel.js
@@ -1,4 +1,4 @@
-import { Schema, mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import contactMsg from '../auxiliary/constants/contactMsg.js';
 import ptrn from '../auxiliary/constants/patterns.js';
 
@@ -26,7 +26,7 @@ const contactSchema = new mongoose.Schema(
     },
 
     owner: {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
   },
